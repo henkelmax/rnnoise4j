5b18bf922e14863be8a16bc442afacd56a19471d
Max Henkel <max@maxhenkel.de>
2025-08-12 20:58:50 +0200

    Target x86_64 v1

--- native/cmake/zig-x86_64-linux-gnu.cmake
+++ native/cmake/zig-x86_64-linux-gnu.cmake
@@ -1,6 +1,6 @@
 set(TARGET_TRIPLE "x86_64-linux-gnu")
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
-set(CPU_ARCHITECTURE_TARGET "x86_64_v2")
+set(CPU_ARCHITECTURE_TARGET "x86_64")
 
 include("${CMAKE_CURRENT_LIST_DIR}/zig-base.cmake")
